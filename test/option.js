738e59ca6ae11c981bae91710be44933aab4d1d2
jonschlinkert <dev@sellside.com>
2014-11-15 14:30:48 -0500

    update tests

--- test/option.js
+++ test/option.js
@@ -43,23 +43,50 @@ describe('option', function () {
       app.options.should.have.property('c', 'd');
       app.options.should.have.property('e', 'f');
     });
+
+    it('should set an option.', function() {
+      app.option('a', 'b');
+      app.options.should.have.property('a');
+    });
+
+    it('should get an option.', function() {
+      app.option('a', 'b');
+      app.option('a').should.equal('b');
+    });
+
+    it('should extend the `options` object.', function() {
+      app.option({x: 'xxx', y: 'yyy', z: 'zzz'});
+      app.option('x').should.equal('xxx');
+      app.option('y').should.equal('yyy');
+      app.option('z').should.equal('zzz');
+    });
+
+    it('options should be on the `options` object.', function() {
+      app.option({x: 'xxx', y: 'yyy', z: 'zzz'});
+      app.options.x.should.equal('xxx');
+      app.options.y.should.equal('yyy');
+      app.options.z.should.equal('zzz');
+    });
+
+    it('should be chainable.', function() {
+      app
+        .option({x: 'xxx', y: 'yyy', z: 'zzz'})
+        .option({a: 'aaa', b: 'bbb', c: 'ccc'});
+
+      app.option('x').should.equal('xxx');
+      app.option('a').should.equal('aaa');
+    });
+
+    it('should extend the `options` object when the first param is a string.', function() {
+      app
+        .option('foo', {x: 'xxx', y: 'yyy', z: 'zzz'})
+        .option('bar', {a: 'aaa', b: 'bbb', c: 'ccc'});
+
+      app.option('foo').should.have.property('x');
+      app.option('bar').should.have.property('a');
+
+      app.options.foo.should.have.property('x');
+      app.options.bar.should.have.property('a');
+    });
   });
-
-  // describe('.disabled()', function () {
-  //   it('should default to false', function () {
-  //     app.disabled('a').should.be.true;
-  //   });
-
-  //   it('should return false when set', function () {
-  //     app.option('a', 'b');
-  //     app.enabled('a').should.be.true;
-  //     app.disabled('a').should.be.false;
-  //   });
-
-  //   it('should return true when set', function () {
-  //     app.option('a', false);
-  //     app.enabled('a').should.be.false;
-  //     app.disabled('a').should.be.true;
-  //   });
-  // });
 });
